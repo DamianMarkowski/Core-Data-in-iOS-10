df40b49661b2b6fed8a3f7d36b216f95dfe84068
Damian Markowski <contact@damianmarkowski.pl>
2017-06-25 13:56:54 +0200

    project created

---
+++ Core Data in iOS 10/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Core Data in iOS 10
+//
+//  Created by Damian Markowski on 25.06.2017.
+//  Copyright © 2017 Damian Markowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
